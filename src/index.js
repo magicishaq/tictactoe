f5197e1d5152a3c7287dfad64675068bc9282e6c
Ishaq Khan <ikhan@uk.nex.global>
2021-01-17 19:08:08 +0000

    completed the tic tac toe game

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import calculateWinner from './calculateWinner'
 function Square ({value, onClick}) {
     return (
         <button className="square" 
-        onClick={() => onClick()}>
+        onClick={onClick}>
           {value}
         </button>
       );
@@ -57,11 +57,12 @@ function Square ({value, onClick}) {
         super(props); 
         this.state = {
             history : [{squares: Array(9).fill(null)}],
-            isXNext : true
+            isXNext : true,
+            stepNumber: 0
         }
     }
     handleClick(i) {
-        const history = this.state.history;
+        const history = this.state.history.slice(0, this.state.stepNumber + 1);
         const current = history[history.length - 1];
         const squares = current.squares.slice();
         if (calculateWinner(squares) || squares[i]) {
@@ -72,12 +73,19 @@ function Square ({value, onClick}) {
           history: history.concat([{
             squares: squares,
           }]),
+          stepNumber: history.length,
           xIsNext: !this.state.xIsNext,
         });
       }
+      jumpTo(step) {
+          this.setState({
+              stepNumber: step,
+              isXNext: (step % 2) === 0
+          })
+      }
     render() {
         const history = this.state.history;
-        const current = history[history.length - 1];
+        const current = history[this.state.stepNumber];
         const winner = calculateWinner(current.squares);
     
         const moves = history.map((step, move) => {
